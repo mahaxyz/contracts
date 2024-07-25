5faa7438de6173654ea6bca4e0fb1b9799e1dcfc
Steven Enamakel <enamakel@maha.xyz>
2024-07-26 01:29:51 +0400

    updated interface

--- contracts/core/stability-pool/MultiStakingRewardsERC4626.sol
+++ contracts/core/stability-pool/MultiStakingRewardsERC4626.sol
@@ -19,6 +19,7 @@ import {
   ERC4626Upgradeable, IERC20
 } from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC4626Upgradeable.sol";
 
+import {IMultiStakingRewardsERC4626} from "../../interfaces/core/IMultiStakingRewardsERC4626.sol";
 import {MulticallUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/MulticallUpgradeable.sol";
 import {ReentrancyGuardUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
@@ -33,37 +34,39 @@ contract MultiStakingRewardsERC4626 is
   AccessControlEnumerableUpgradeable,
   ERC4626Upgradeable,
   ReentrancyGuardUpgradeable,
-  MulticallUpgradeable
+  MulticallUpgradeable,
+  IMultiStakingRewardsERC4626
 {
   using SafeERC20 for IERC20;
 
+  /// @inheritdoc IMultiStakingRewardsERC4626
   bytes32 public DISTRIBUTOR_ROLE;
 
-  /// @notice Time at which distribution ends
+  /// @inheritdoc IMultiStakingRewardsERC4626
   mapping(IERC20 reward => uint256) public periodFinish;
 
-  /// @notice Reward per second given to the staking contract, split among the staked tokens
+  /// @inheritdoc IMultiStakingRewardsERC4626
   mapping(IERC20 reward => uint256) public rewardRate;
 
-  /// @notice Duration of the reward distribution
+  /// @inheritdoc IMultiStakingRewardsERC4626
   uint256 public rewardsDuration;
 
-  /// @notice Last time `rewardPerTokenStored` was updated
+  /// @inheritdoc IMultiStakingRewardsERC4626
   mapping(IERC20 reward => uint256) public lastUpdateTime;
 
-  /// @notice Helps to compute the amount earned by someone
-  /// Cumulates rewards accumulated for one token since the beginning.
-  /// Stored as a uint so it is actually a float times the base of the reward token
+  /// @inheritdoc IMultiStakingRewardsERC4626
   mapping(IERC20 reward => uint256) public rewardPerTokenStored;
 
-  /// @notice Stores for each account the `rewardPerToken`: we do the difference
-  /// between the current and the old value to compute what has been earned by an account
+  /// @inheritdoc IMultiStakingRewardsERC4626
   mapping(IERC20 reward => mapping(address who => uint256)) public userRewardPerTokenPaid;
 
-  /// @notice Stores for each account the accumulated rewards
+  /// @inheritdoc IMultiStakingRewardsERC4626
   mapping(IERC20 reward => mapping(address who => uint256 rewards)) public rewards;
 
+  /// @inheritdoc IMultiStakingRewardsERC4626
   IERC20 public rewardToken1;
+
+  /// @inheritdoc IMultiStakingRewardsERC4626
   IERC20 public rewardToken2;
 
   // ============================ Constructor ====================================
@@ -119,16 +122,12 @@ contract MultiStakingRewardsERC4626 is
 
   // ============================ View functions =================================
 
-  /// @notice Queries the last timestamp at which a reward was distributed
-  /// @dev Returns the current timestamp if a reward is being distributed and the end of the staking
-  /// period if staking is done
+  /// @inheritdoc IMultiStakingRewardsERC4626
   function lastTimeRewardApplicable(IERC20 token) public view returns (uint256) {
     return Math.min(block.timestamp, periodFinish[token]);
   }
 
-  /// @notice Used to actualize the `rewardPerTokenStored`
-  /// @dev It adds to the reward per token: the time elapsed since the `rewardPerTokenStored` was
-  /// last updated multiplied by the `rewardRate` divided by the number of tokens
+  /// @inheritdoc IMultiStakingRewardsERC4626
   function rewardPerToken(IERC20 token) public view returns (uint256) {
     if (totalSupply() == 0) {
       return rewardPerTokenStored[token];
@@ -137,11 +136,7 @@ contract MultiStakingRewardsERC4626 is
       + (((lastTimeRewardApplicable(token) - lastUpdateTime[token]) * rewardRate[token] * 1e18) / totalSupply());
   }
 
-  /// @notice Returns how much a given account earned rewards
-  /// @param account Address for which the request is made
-  /// @return How much a given account earned rewards
-  /// @dev It adds to the rewards the amount of reward earned since last time that is the difference
-  /// in reward per token from now and last time multiplied by the number of tokens staked by the person
+  /// @inheritdoc IMultiStakingRewardsERC4626
   function earned(IERC20 token, address account) public view returns (uint256) {
     return (balanceOf(account) * (rewardPerToken(token) - userRewardPerTokenPaid[token][account])) / 1e18
       + rewards[token][account];
@@ -167,7 +162,7 @@ contract MultiStakingRewardsERC4626 is
     super._deposit(caller, receiver, assets, shares);
   }
 
-  /// @notice Triggers a payment of the reward earned to the msg.sender
+  /// @inheritdoc IMultiStakingRewardsERC4626
   function getReward(address who, IERC20 token) public nonReentrant {
     _updateReward(token, who);
     uint256 reward = rewards[token][who];
@@ -180,9 +175,7 @@ contract MultiStakingRewardsERC4626 is
 
   // ====================== Restricted Functions =================================
 
-  /// @notice Adds rewards to be distributed
-  /// @param reward Amount of reward tokens to distribute
-  /// @dev This reward will be distributed during `rewardsDuration` set previously
+  /// @inheritdoc IMultiStakingRewardsERC4626
   function notifyRewardAmount(IERC20 token, uint256 reward) external onlyRole(DISTRIBUTOR_ROLE) nonReentrant {
     _updateReward(token, address(0));
     token.safeTransferFrom(msg.sender, address(this), reward);
