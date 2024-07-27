172976e512df4da7dca14f93f1552dfd14d89336
Steven Enamakel <enamakel@maha.xyz>
2024-07-27 23:22:36 +0400

    added deploy script for usdt

---
+++ scripts/create2/psm-usdt.ts
@@ -0,0 +1,77 @@
+import hre, { ethers, network } from "hardhat";
+import { buildBytecode } from "./create2";
+
+async function main() {
+  const factory = await hre.ethers.getContractFactory("MAHAProxy");
+  const impl = await hre.ethers.getContractFactory("PegStabilityModule");
+
+  const implArgs = [
+    "0x6900057428C99Fb373397D657Beb40D92D8aC97f", // address _zai,
+    "0xdac17f958d2ee523a2206206994597c13d831ec7", // address _collateral,
+    "0xe5159e75ba5f1C9E386A3ad2FC7eA75c14629572", // address _governance,
+    1e6, // uint256 _newRate,
+    100000000n * 10n ** 6n, // uint256 _supplyCap,
+    100000000n * 10n ** 18n, // uint256 _debtCap,
+    0, // uint256 _mintFeeBps,
+    300, // uint256 _redeemFeeBps,
+    "0x6357EDbfE5aDA570005ceB8FAd3139eF5A8863CC", // address _feeDestination
+  ];
+
+  const initData = impl.interface.encodeFunctionData("initialize", implArgs);
+
+  const constructorArgs: any[] = [
+    "0x401139484880aD01Db2E75cA11d26FD7045e13f4",
+    "0x69000f2f879ee598ddf16c6c33cfc4f2d983b6bd",
+    initData,
+  ];
+  const salt =
+    "0x471a78fa81e1c16f74411acf0ca44ca98b2257604cc0ae51c54cbd9f939131c8";
+  const address = "0x69000a93c8acf8126d6ef5b1054c2695744ca4ee";
+
+  const [wallet] = await hre.ethers.getSigners();
+  const deployer = await hre.ethers.getContractAt(
+    "Deployer",
+    "0xc07c1980C87bfD5de0DC77f90Ce6508c1C0795C3"
+  );
+
+  const bytecode = buildBytecode(
+    ["address", "address", "bytes"],
+    constructorArgs,
+    factory.bytecode
+  );
+
+  const txPopulated = await deployer.deploy.populateTransaction(
+    bytecode,
+    ethers.id(salt)
+  );
+  const tx = await wallet.sendTransaction(txPopulated);
+
+  const txR = await tx.wait(1);
+  console.log(txR?.logs);
+
+  if (network.name !== "hardhat") {
+    await hre.deployments.save("PegStabilityModule-USDC", {
+      address: address,
+      args: implArgs,
+      abi: impl.interface.format(true),
+    });
+
+    await hre.deployments.save("PegStabilityModule-USDC-Proxy", {
+      address: address,
+      args: constructorArgs,
+      abi: factory.interface.format(true),
+    });
+
+    await hre.run("verify:verify", {
+      address: address,
+      constructorArguments: constructorArgs,
+    });
+  }
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
---
+++ scripts/guess/psm-usdt.ts
@@ -0,0 +1,66 @@
+// import
+import { ethers } from "ethers";
+import { getCreate2Address } from "../create2/create2";
+
+// declare deployment parameters
+import contractArtifact from "../../artifacts/contracts/governance/MAHAProxy.sol/MAHAProxy.json";
+import implArtificat from "../../artifacts/contracts/core/psm/PegStabilityModule.sol/PegStabilityModule.json";
+
+// @ts-ignore
+const constructorTypes = contractArtifact.abi
+  .find((v) => v.type === "constructor")
+  ?.inputs.map((t) => t.type);
+
+const impl = new ethers.Contract(
+  "0x401139484880aD01Db2E75cA11d26FD7045e13f4",
+  implArtificat.abi
+);
+
+const initData = impl.interface.encodeFunctionData("initialize", [
+  "0x6900057428C99Fb373397D657Beb40D92D8aC97f", // address _zai,
+  "0xdac17f958d2ee523a2206206994597c13d831ec7", // address _collateral,
+  "0xe5159e75ba5f1C9E386A3ad2FC7eA75c14629572", // address _governance,
+  1e6, // uint256 _newRate,
+  100000000n * 10n ** 6n, // uint256 _supplyCap,
+  100000000n * 10n ** 18n, // uint256 _debtCap,
+  0, // uint256 _mintFeeBps,
+  300, // uint256 _redeemFeeBps,
+  "0x6357EDbfE5aDA570005ceB8FAd3139eF5A8863CC", // address _feeDestination
+]);
+
+const factoryAddress = "0xc07c1980C87bfD5de0DC77f90Ce6508c1C0795C3";
+const constructorArgs: any[] = [
+  "0x401139484880aD01Db2E75cA11d26FD7045e13f4", // implementation
+  "0x69000f2f879ee598ddf16c6c33cfc4f2d983b6bd", // proxyadmin
+  initData, // init data
+];
+
+console.log("constructor parameters", constructorTypes, constructorArgs);
+
+const job = () => {
+  let i = 0;
+
+  while (true) {
+    const salt = ethers.id("" + i);
+    // Calculate contract address
+    const computedAddress = getCreate2Address({
+      salt: salt,
+
+      factoryAddress,
+      contractBytecode: contractArtifact.bytecode,
+      constructorTypes: constructorTypes,
+      constructorArgs: constructorArgs,
+    });
+
+    if (computedAddress.startsWith("0x69000")) {
+      console.log("found the right salt hash");
+      console.log("salt", salt, computedAddress);
+      break;
+    }
+
+    if (i % 100000 == 0) console.log(i, "salt", salt, computedAddress);
+    i++;
+  }
+};
+
+job();
