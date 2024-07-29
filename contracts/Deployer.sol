2dbc7038c979d4c78c289ab2cb003581d6a99bb2
Steven Enamakel <enamakel@maha.xyz>
2024-07-29 23:15:39 +0400

    added with revert for deployer

--- contracts/Deployer.sol
+++ contracts/Deployer.sol
@@ -25,4 +25,16 @@ contract Deployer {
 
     emit Deploy(addr);
   }
+
+  function deployWithAssert(bytes memory bytecode, uint256 _salt, address target) external {
+    address addr;
+    assembly {
+      addr := create2(0, add(bytecode, 0x20), mload(bytecode), _salt)
+      if iszero(extcodesize(addr)) { revert(0, 0) }
+    }
+
+    require(addr == target, "Deployer: INVALID_TARGET");
+
+    emit Deploy(addr);
+  }
 }
