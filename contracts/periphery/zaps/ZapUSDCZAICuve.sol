a37d1511b37d31b6b03994570239e5f5268408cc
Steven Enamakel <enamakel@maha.xyz>
2024-07-29 22:45:27 +0400

    finsihed the curve fi zap functionalitiy

--- contracts/periphery/zaps/ZapUSDCZAICuve.sol
+++ contracts/periphery/zaps/ZapUSDCZAICuve.sol
@@ -13,95 +13,114 @@
 
 pragma solidity 0.8.21;
 
-import {IERC20, IPegStabilityModule} from "../../interfaces/core/IPegStabilityModule.sol";
-import {ILocker} from "../../interfaces/governance/ILocker.sol";
+import {IPegStabilityModule} from "../../interfaces/core/IPegStabilityModule.sol";
+import {ICurveStableSwapNG} from "../../interfaces/periphery/ICurveStableSwapNG.sol";
+
+import {IERC20Metadata} from "@openzeppelin/contracts/interfaces/IERC20Metadata.sol";
 import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 
 /**
  * @title ZapLockerLP
- * @dev This contract allows users to perform a Zap operation by swapping usdc for zai tokens, adding liquidity to curve
- * LP, and staking LP
- * tokens.
+ * @dev This contract allows users to perform a Zap operation by swapping collateral for zai tokens, adding liquidity to
+ * curve LP, and staking the LP tokens.
  */
 contract ZapUSDCZAICuve {
-  address public odos;
   IERC4626 public staking;
-  IERC20 public zai;
-  IERC20 public usdc;
-  IERC20 public lp;
+
+  ICurveStableSwapNG public pool;
+
+  IERC20Metadata public zai;
+
+  IERC20Metadata public collateral;
+
   IPegStabilityModule public psm;
+
+  uint256 private decimalOffset;
 
   address private me;
 
   error OdosSwapFailed();
-  error USDCTransferFailed();
+  error CollateralTransferFailed();
   error ZaiTransferFailed();
+
+  event Zapped(
+    address indexed user, uint256 indexed collateralAmount, uint256 indexed zaiAmount, uint256 newStakedAmount
+  );
 
   /**
    * @dev Initializes the contract with the required contracts
    */
-  constructor(address _odos, address _staking, address _zai, address _usdc, address _lp, address _psm) {
-    odos = _odos;
+  constructor(address _staking, address _psm) {
     staking = IERC4626(_staking);
-    zai = IERC20(_zai);
-    usdc = IERC20(_usdc);
-    lp = IERC20(_lp);
     psm = IPegStabilityModule(_psm);
 
+    pool = ICurveStableSwapNG(staking.asset());
+    zai = IERC20Metadata(address(psm.zai()));
+    collateral = IERC20Metadata(address(psm.collateral()));
+
+    decimalOffset = 10 ** (18 - collateral.decimals());
+
     // give approvals
-    zai.approve(_odos, type(uint256).max);
-    usdc.approve(_odos, type(uint256).max);
-    lp.approve(_staking, type(uint256).max);
+    zai.approve(address(pool), type(uint256).max);
+    collateral.approve(address(pool), type(uint256).max);
+    pool.approve(_staking, type(uint256).max);
 
     me = address(this);
   }
 
-  function zapUsdcIntoLP(uint256 usdcAmount, bytes calldata odosSwapData) external {
+  function zapUsdcIntoLP(uint256 collateralAmount, uint256 minLpAmount) external {
     // fetch tokens
-    usdc.transferFrom(msg.sender, me, usdcAmount);
+    collateral.transferFrom(msg.sender, me, collateralAmount);
 
-    // convert 50% usdc for zai
-    psm.mint(address(this), usdcAmount * 1e12 / 2);
+    // convert 50% collateral for zai
+    uint256 zaiAmount = collateralAmount * decimalOffset / 2;
+    psm.mint(address(this), zaiAmount);
 
     // add liquidity
-    // odos should be able to swap into LP tokens directly.
-    (bool success,) = odos.call(odosSwapData);
-    if (!success) revert OdosSwapFailed();
+    uint256[] memory amounts = new uint256[](2);
+    amounts[0] = zaiAmount;
+    amounts[1] = collateralAmount / 2;
+    pool.add_liquidity(amounts, minLpAmount, address(this));
 
     // we now have LP tokens; deposit into staking contract for the user
-    staking.deposit(lp.balanceOf(address(this)), msg.sender);
+    staking.deposit(pool.balanceOf(address(this)), msg.sender);
 
     // sweep any dust
     sweep();
+
+    emit Zapped(msg.sender, collateralAmount / 2, zaiAmount, pool.balanceOf(msg.sender));
   }
 
-  function zapZaiUsdcIntoLP(uint256 zaiAmount, uint256 usdcAmount, bytes calldata odosSwapData) external {
+  function zapZaiUsdcIntoLP(uint256 zaiAmount, uint256 collateralAmount, uint256 minLpAmount) external {
     // fetch tokens
     if (zaiAmount > 0) zai.transferFrom(msg.sender, me, zaiAmount);
-    if (usdcAmount > 0) usdc.transferFrom(msg.sender, me, usdcAmount);
+    if (collateralAmount > 0) collateral.transferFrom(msg.sender, me, collateralAmount);
 
     // add liquidity
-    // odos should be able to swap into LP tokens directly.
-    (bool success,) = odos.call(odosSwapData);
-    if (!success) revert OdosSwapFailed();
+    uint256[] memory amounts = new uint256[](2);
+    amounts[0] = zaiAmount;
+    amounts[1] = collateralAmount;
+    pool.add_liquidity(amounts, minLpAmount, address(this));
 
     // we now have LP tokens; deposit into staking contract for the user
-    staking.deposit(lp.balanceOf(address(this)), msg.sender);
+    staking.deposit(pool.balanceOf(address(this)), msg.sender);
 
     // sweep any dust
     sweep();
+
+    emit Zapped(msg.sender, collateralAmount, zaiAmount, pool.balanceOf(msg.sender));
   }
 
   function sweep() public {
     uint256 zaiB = zai.balanceOf(address(this));
-    uint256 usdcB = usdc.balanceOf(address(this));
+    uint256 collateralB = collateral.balanceOf(address(this));
 
     if (zaiB > 0 && !zai.transfer(msg.sender, zaiB)) {
       revert ZaiTransferFailed();
     }
 
-    if (usdcB > 0 && !usdc.transfer(msg.sender, usdcB)) {
-      revert USDCTransferFailed();
+    if (collateralB > 0 && !collateral.transfer(msg.sender, collateralB)) {
+      revert CollateralTransferFailed();
     }
   }
 }
