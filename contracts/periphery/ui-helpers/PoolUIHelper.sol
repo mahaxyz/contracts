cdd2f236607fdb5c9c737670a8aed50a84a40693
Steven Enamakel <enamakel@maha.xyz>
2024-08-08 14:09:59 +0400

    wrote a PoolUIHelper for staking pools and APR

---
+++ contracts/periphery/ui-helpers/PoolUIHelper.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: GPL-3.0
+
+// ███╗   ███╗ █████╗ ██╗  ██╗ █████╗
+// ████╗ ████║██╔══██╗██║  ██║██╔══██╗
+// ██╔████╔██║███████║███████║███████║
+// ██║╚██╔╝██║██╔══██║██╔══██║██╔══██║
+// ██║ ╚═╝ ██║██║  ██║██║  ██║██║  ██║
+// ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
+
+// Website: https://maha.xyz
+// Discord: https://discord.gg/mahadao
+// Twitter: https://twitter.com/mahaxyz_
+
+pragma solidity 0.8.21;
+
+import {IMultiStakingRewardsERC4626} from "../../interfaces/core/IMultiStakingRewardsERC4626.sol";
+import {ICurveStableSwapNG} from "../../interfaces/periphery/ICurveStableSwapNG.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20, IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
+
+contract PoolUIHelper {
+  IERC20 maha;
+  IERC20 zai;
+  IERC20 usdc;
+
+  struct PoolInfoResponse {
+    uint256 mahaAprE8;
+    uint256 mahaRewardsPerYearE18;
+    uint256 mahaTotalSupply;
+    uint256 mahaUserBalance;
+    uint256 poolUsdTVLE8;
+    uint256 usdcAprE8;
+    uint256 usdcRewardsPerYearE6;
+    uint256 usdcTotalSupply;
+    uint256 usdcUserBalance;
+    uint256 userShareE18;
+    uint256 zaiTotalSupply;
+    uint256 zaiUserBalance;
+  }
+
+  constructor(address _maha, address _zai, address _usdc) {
+    maha = IERC20(_maha);
+    zai = IERC20(_zai);
+    usdc = IERC20(_usdc);
+  }
+
+  function getPoolInfo(
+    address _stakingPool,
+    uint256 _mahaPriceE8,
+    address _who
+  ) external view returns (PoolInfoResponse memory res) {
+    IMultiStakingRewardsERC4626 stakingPool = IMultiStakingRewardsERC4626(_stakingPool);
+    IERC4626 stakingPool4626 = IERC4626(_stakingPool);
+    ICurveStableSwapNG pool = ICurveStableSwapNG(IERC4626(_stakingPool).asset());
+
+    res.mahaTotalSupply = maha.balanceOf(address(pool));
+    res.zaiTotalSupply = zai.balanceOf(address(pool));
+    res.usdcTotalSupply = usdc.balanceOf(address(pool));
+
+    res.poolUsdTVLE8 =
+      (res.mahaTotalSupply * _mahaPriceE8 / 1e8 + res.usdcTotalSupply * 1e12 + res.zaiTotalSupply) / 1e10;
+
+    res.userShareE18 = stakingPool4626.balanceOf(_who) * 1e18 / stakingPool4626.totalSupply();
+
+    res.mahaUserBalance = res.mahaTotalSupply * res.userShareE18 / 1e18;
+    res.usdcUserBalance = res.usdcTotalSupply * res.userShareE18 / 1e18;
+    res.zaiUserBalance = res.zaiTotalSupply * res.userShareE18 / 1e18;
+
+    res.mahaRewardsPerYearE18 = stakingPool.rewardRate(maha) * 365 days;
+    res.usdcRewardsPerYearE6 = stakingPool.rewardRate(usdc) * 365 days;
+    res.mahaAprE8 = res.mahaRewardsPerYearE18 * _mahaPriceE8 / (res.poolUsdTVLE8 * 1e10);
+    res.usdcAprE8 = res.usdcRewardsPerYearE6 * 1e10 / res.poolUsdTVLE8;
+  }
+}
---
+++ test/foundry/periphery/uihelpers/PoolUIHelperTest.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: GPL-3.0
+
+// ███╗   ███╗ █████╗ ██╗  ██╗ █████╗
+// ████╗ ████║██╔══██╗██║  ██║██╔══██╗
+// ██╔████╔██║███████║███████║███████║
+// ██║╚██╔╝██║██╔══██║██╔══██║██╔══██║
+// ██║ ╚═╝ ██║██║  ██║██║  ██║██║  ██║
+// ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
+
+// Website: https://maha.xyz
+// Discord: https://discord.gg/mahadao
+// Twitter: https://twitter.com/mahaxyz_
+
+pragma solidity 0.8.21;
+
+import {PegStabilityModule} from "../../../../contracts/core/psm/PegStabilityModule.sol";
+import {StakingLPRewards} from "../../../../contracts/periphery/staking/StakingLPRewards.sol";
+
+import {IERC20, PoolUIHelper} from "../../../../contracts/periphery/ui-helpers/PoolUIHelper.sol";
+import {BaseZaiTest, console} from "../../base/BaseZaiTest.sol";
+
+contract PoolUIHelperTest is BaseZaiTest {
+  StakingLPRewards internal staking;
+
+  string MAINNET_RPC_URL = vm.envString("MAINNET_RPC_URL");
+
+  function test_zap_fork_usdc() public {
+    uint256 mainnetFork = vm.createFork(MAINNET_RPC_URL);
+    vm.selectFork(mainnetFork);
+    vm.rollFork(20_483_051);
+
+    IERC20 _usdc = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    IERC20 _zai = IERC20(0x69000405f9DcE69BD4Cbf4f2865b79144A69BFE0);
+    IERC20 _maha = IERC20(0x745407c86DF8DB893011912d3aB28e68B62E49B0);
+
+    // USDC/USDz staking
+    IERC20 _staking = IERC20(0x154F52B347D8E48b8DbD8D8325Fe5bb45AAdCCDa);
+
+    address user = 0x1F09Ec21d7fd0A21879b919bf0f9C46e6b85CA8b;
+    address usdcWhale = 0x95Ba4cF87D6723ad9C0Db21737D862bE80e93911;
+
+    PoolUIHelper helper = new PoolUIHelper(
+      address(_maha), // maha
+      address(_zai), // zai
+      address(_usdc) // usdc
+    );
+
+    vm.prank(0x6357EDbfE5aDA570005ceB8FAd3139eF5A8863CC);
+    StakingLPRewards(address(_staking)).grantRole(keccak256("DISTRIBUTOR_ROLE"), usdcWhale);
+
+    vm.startPrank(usdcWhale);
+    _usdc.approve(address(_staking), type(uint256).max);
+    StakingLPRewards(address(_staking)).notifyRewardAmount(_usdc, 100e6);
+
+    PoolUIHelper.PoolInfoResponse memory res = helper.getPoolInfo(address(_staking), 91 * 1e6, user); // 0.91$ per maha
+
+    assertEq(res.usdcTotalSupply, 15 * 1e6, "!usdcTotalSupply");
+    assertEq(res.zaiTotalSupply, 15 * 1e18, "!zaiTotalSupply");
+    assertEq(res.poolUsdTVLE8, 30 * 1e8, "!poolUsdTVL");
+    assertApproxEqAbs(res.usdcRewardsPerYearE6, 52 * 100e6, 5e6, "!usdcRewardsPerYear");
+    assertApproxEqAbs(res.usdcAprE8, 173 * 1e8, 1e8, "!usdcAprE8");
+  }
+}
