22773ba4a2179d3dc7313098ef89537945b1ac66
Steven Enamakel <enamakel@maha.xyz>
2024-08-23 08:44:53 +0400

    added config to save MAHA files

--- deploy/oft/deploy-oft-maha.ts
+++ deploy/oft/deploy-oft-maha.ts
@@ -3,12 +3,24 @@ import { deployContract } from "../../scripts/utils";
 import { config } from "../../tasks/layerzero/config";
 
 async function main(hre: HardhatRuntimeEnvironment) {
-  await deployContract(
+  const contract = await deployContract(
     hre,
     "LayerZeroCustomOFT",
     ["MAHA.xyz", "MAHA", config[hre.network.name].libraries.endpoint],
     "MAHAOFT"
   );
+
+  const zai = await hre.ethers.getContractAt(
+    "LayerZeroCustomOFT",
+    contract.address
+  );
+
+  if (!(await hre.deployments.getOrNull("MAHA"))) {
+    await hre.deployments.save("MAHA", {
+      abi: zai.interface.format(true),
+      address: contract.address,
+    });
+  }
 }
 
 main.tags = ["MAHAOFT"];
