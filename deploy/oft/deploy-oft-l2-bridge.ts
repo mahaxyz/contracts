6b57151f3c2afd700bea51d51f7069fd96b2f062
Steven Enamakel <enamakel@maha.xyz>
2024-08-27 08:54:55 +0400

    added arbitrum

--- deploy/oft/deploy-oft-l2-bridge.ts
+++ deploy/oft/deploy-oft-l2-bridge.ts
@@ -1,28 +1,31 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { deployContract, deployProxy } from "../../scripts/utils";
-import { config } from "../../tasks/layerzero/config";
+import { deployProxy, waitForTx } from "../../scripts/utils";
 import { get } from "../../scripts/guess/_helpers";
-import { zeroPadValue } from "ethers";
+import { ZeroAddress, zeroPadValue } from "ethers";
 
 async function main(hre: HardhatRuntimeEnvironment) {
   const l1Contract = get("L1BridgeCollateralL0", "mainnet");
-  const zai = await hre.deployments.get("ZaiStablecoinOFT");
+  const zaiD = await hre.deployments.get("ZaiStablecoinOFT");
   const usdc = await hre.deployments.get("USDC");
-  const stargate = await hre.deployments.get("StargateUSDCPool");
+  const stargate = await hre.deployments.getOrNull("StargateUSDCPool");
   const { deployments, getNamedAccounts } = hre;
   const { deployer } = await getNamedAccounts();
 
   const { address: proxyAdmin } = await deployments.get("ProxyAdmin");
 
+  const zai = await hre.ethers.getContractAt(
+    "ZaiOFTWithRestaking",
+    zaiD.address
+  );
   const l1ContractPadded = zeroPadValue(l1Contract, 32);
 
-  await deployProxy(
+  const restaker = await deployProxy(
     hre,
     "L2DepositCollateralL0",
     [
-      zai.address,
+      zaiD.address,
       usdc.address, // IERC20 _depositToken,
-      stargate.address, // IStargate _stargate,
+      stargate ? stargate.address : ZeroAddress, // IStargate _stargate,
       l1ContractPadded, // bytes32 _bridgeTargetAddress,
       deployer, // address _governance,
       1e6, // uint256 _rate,
@@ -31,6 +34,8 @@ async function main(hre: HardhatRuntimeEnvironment) {
     proxyAdmin,
     "L2DepositCollateralL0"
   );
+
+  await waitForTx(await zai.setRestaker(restaker.address));
 }
 
 main.tags = ["L2DepositCollateralL0"];
