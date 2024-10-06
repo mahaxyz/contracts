d495ebc12f7cab048ca8a359e60fb02d11a01973
Steven Enamakel <enamakel@maha.xyz>
2024-10-07 01:02:08 +0400

    cleaned up cron job

--- contracts/periphery/crons/L1RestakingSweeperCron.sol
+++ contracts/periphery/crons/L1RestakingSweeperCron.sol
@@ -13,39 +13,22 @@
 
 pragma solidity 0.8.21;
 
-import {IStargate} from "../../interfaces/periphery/layerzero/IStargate.sol";
-import {IOFT, MessagingFee, SendParam} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/interfaces/IOFT.sol";
-
-import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 
 interface IL1BridgeCollateralL0 {
   function process() external;
 }
 
-contract L1RestakingSweeperCron is OwnableUpgradeable {
-  address public gelatoooooo;
+contract L1RestakingSweeperCron is Ownable {
   address public usdc;
   uint256 public limit;
   IL1BridgeCollateralL0 public depositCollateralL0;
 
-  function initialize(
-    address _usdc,
-    uint256 _limit,
-    address _depositCollateralL0,
-    address _governance
-  ) public reinitializer(1) {
-    __Ownable_init(msg.sender);
-
+  constructor(address _usdc, uint256 _limit, address _depositCollateralL0, address _governance) Ownable(_governance) {
     usdc = _usdc;
     limit = _limit;
     depositCollateralL0 = IL1BridgeCollateralL0(_depositCollateralL0);
-
-    _transferOwnership(_governance);
-  }
-
-  receive() external payable {
-    // nothing
   }
 
   function setSweepLimit(uint256 _limit) public onlyOwner {
@@ -58,15 +41,5 @@ contract L1RestakingSweeperCron is OwnableUpgradeable {
 
   function shouldExecute() public view returns (bool) {
     return usdcToSweep() >= limit;
-  }
-
-  function execute() public payable {
-    require(msg.sender == owner() || msg.sender == gelatoooooo, "who dis?");
-    IERC20(usdc).balanceOf(address(depositCollateralL0));
-    depositCollateralL0.process();
-  }
-
-  function refundETH() public onlyOwner {
-    payable(owner()).transfer(address(this).balance);
   }
 }
