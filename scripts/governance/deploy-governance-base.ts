5ed559435411cb0fa12d3454ee80481157b00198
obchain <riteshnikhoriya94@gmail.com>
2024-10-29 12:45:11 +0530

    refactor: prettier + logs

--- scripts/governance/deploy-governance-base.ts
+++ scripts/governance/deploy-governance-base.ts
@@ -6,6 +6,7 @@ async function main() {
   const MAHA_BASE = "0x554bba833518793056CF105E66aBEA330672c0dE";
   const WETH_BASE = "0x4200000000000000000000000000000000000006";
   const REWARD_DURATION = 86400 * 7; // 7 Days
+
   const TransparentProxy = await ethers.getContractFactory(
     "TransparentUpgradeableProxy"
   );
@@ -14,43 +15,60 @@ async function main() {
     "OmnichainStakingToken"
   );
 
+  // Deploy the implementations
   const omnichainStakingTokenImpl = await OmnichainStakingToken.deploy();
   const lockerTokenImpl = await LockerToken.deploy();
 
   await omnichainStakingTokenImpl.waitForDeployment();
   await lockerTokenImpl.waitForDeployment();
 
+  console.log(
+    `Omnichain Staking Implementation: ${await omnichainStakingTokenImpl.getAddress()}`
+  );
+  console.log(
+    `Locker Token Implementation: ${await lockerTokenImpl.getAddress()}`
+  );
+
+  // Deploy proxies
+  const omnichainStakingTokenProxy = await TransparentProxy.deploy(
+    await omnichainStakingTokenImpl.getAddress(),
+    SAFE,
+    "0x"
+  );
+  const lockerTokenProxy = await TransparentProxy.deploy(
+    await lockerTokenImpl.getAddress(),
+    SAFE,
+    "0x"
+  );
+
+  await omnichainStakingTokenProxy.waitForDeployment();
+  await lockerTokenProxy.waitForDeployment();
 
   console.log(
-    `Omnichain Staking Implementation ${omnichainStakingTokenImpl.target}`
+    `Omnichain Staking Proxy: ${await omnichainStakingTokenProxy.getAddress()}`
   );
-  console.log(`Locker Token Implementation ${lockerTokenImpl.target}`);
+  console.log(`Locker Token Proxy: ${await lockerTokenProxy.getAddress()}`);
 
-  // Now proxies
-  const OmnichainStakingTokenProxy = await TransparentProxy.deploy(
-    omnichainStakingTokenImpl.target,
-    SAFE,
-    "0x"
-  );
-  const LockerTokenProxy = await TransparentProxy.deploy(
-    lockerTokenImpl.target,
-    SAFE,
-    "0x"
-  );
-
+  // Interacting through proxies
   const omnichainStakingToken = await ethers.getContractAt(
     "OmnichainStakingToken",
-    OmnichainStakingTokenProxy.target
-  );
-  const lockerToken = await ethers.getContractAt(
-    "LockerToken",
-    LockerTokenProxy.target
+    await omnichainStakingTokenProxy.getAddress()
   );
 
-  // Initialize the contract
-  await lockerToken.init(MAHA_BASE, omnichainStakingToken.target, ZeroAddress);
+  const lockerToken = await ethers.getContractAt(
+    "LockerToken",
+    await lockerTokenProxy.getAddress()
+  );
+
+  // Initialize the contracts
+  await lockerToken.init(
+    MAHA_BASE,
+    await omnichainStakingToken.getAddress(),
+    ZeroAddress
+  );
+
   await omnichainStakingToken.init(
-    lockerToken.target,
+    await lockerToken.getAddress(),
     WETH_BASE,
     MAHA_BASE,
     REWARD_DURATION,
@@ -59,19 +77,24 @@ async function main() {
   );
 
   if (network.name !== "hardhat") {
-    // Verify the implementation
-    await run("verify:verify", { address: lockerTokenImpl.target });
-    await run("verify:verify", { address: omnichainStakingTokenImpl.target });
-
-    // Verify the proxy
+    // Verify the implementations
+    await run("verify:verify", { address: await lockerTokenImpl.getAddress() });
     await run("verify:verify", {
-      address: lockerToken.target,
-      constructorArguments: [lockerTokenImpl.target, SAFE, "0x"],
+      address: await omnichainStakingTokenImpl.getAddress(),
     });
 
+    // Verify the proxies
     await run("verify:verify", {
-      address: omnichainStakingToken.target,
-      constructorArguments: [omnichainStakingTokenImpl.target, SAFE, "0x"],
+      address: await lockerTokenProxy.getAddress(),
+      constructorArguments: [await lockerTokenImpl.getAddress(), SAFE, "0x"],
+    });
+    await run("verify:verify", {
+      address: await omnichainStakingTokenProxy.getAddress(),
+      constructorArguments: [
+        await omnichainStakingTokenImpl.getAddress(),
+        SAFE,
+        "0x",
+      ],
     });
   }
 }
