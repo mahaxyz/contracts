24e734bc31a78ed64d76f681139bf1f82353036a
Steven Enamakel <enamakel@maha.xyz>
2024-11-23 17:12:04 +0400

    added odos for zap contract

--- contracts/periphery/zaps/implementations/ethereum/ZapSafetyPool.sol
+++ contracts/periphery/zaps/implementations/ethereum/ZapSafetyPool.sol
@@ -34,16 +34,19 @@ contract ZapSafetyPool {
   /// @dev The address of this contract
   address private me;
 
+  address public odos;
+
   error TokenTransferFailed();
   error ZaiTransferFailed();
 
   /**
    * @dev Initializes the contract with the required contracts
    */
-  constructor(address _safetyPool, address _zai) {
+  constructor(address _safetyPool, address _zai, address _odos) {
     safetyPool = IERC4626(_safetyPool);
     zai = IERC20(_zai);
     me = address(this);
+    odos = _odos;
     zai.approve(_safetyPool, type(uint256).max);
   }
 
@@ -61,6 +64,36 @@ contract ZapSafetyPool {
     psm.mint(me, zaiToMint);
 
     safetyPool.deposit(zaiToMint, msg.sender);
+
+    // sweep any dust
+    sweep(asset);
+  }
+
+  function zapIntoSafetyPoolWithOdos(
+    IPegStabilityModule psm,
+    IERC20 swapAsset,
+    uint256 swapAmount,
+    uint256 minShares,
+    bytes memory odosCallData
+  ) external payable {
+    if (swapAsset != IERC20(address(0))) {
+      swapAsset.safeTransferFrom(msg.sender, me, swapAmount);
+    }
+
+    // swap on odos
+    swapAsset.approve(odos, swapAmount);
+    (bool success,) = odos.call{value: msg.value}(odosCallData);
+    require(success, "odos call failed");
+
+    // mint ZAI
+    IERC20 asset = IERC20(psm.collateral());
+    uint256 zaiToMint = psm.mintAmountIn(asset.balanceOf(me));
+    asset.approve(address(psm), type(uint256).max);
+    psm.mint(me, zaiToMint);
+
+    // mint sZAI
+    uint256 shares = safetyPool.deposit(zaiToMint, msg.sender);
+    require(shares >= minShares, "insufficient tokens minted");
 
     // sweep any dust
     sweep(asset);
