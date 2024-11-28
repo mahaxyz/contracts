1c9254f0dba46b97bf67b543c019d6a470dfb872
obchain <riteshnikhoriya94@gmail.com>
2024-11-28 15:58:13 +0530

    fix: added correct proxy admin

--- deploy/migrator/deploy-migrator.ts
+++ deploy/migrator/deploy-migrator.ts
@@ -3,15 +3,16 @@ import { deployProxy } from "../../scripts/utils";
 
 async function main(hre: HardhatRuntimeEnvironment) {
   const LOCKER = "0x7DF7505aa7cfAb3AC1A8D1EC225f2fafe5f04c74";
-  const MERKLE_ROOT = "0x9547986592f11cd509fb211d694eb556636e7ec75a5d879d404c98ecf6d211fb";
-  const { deployments, getNamedAccounts } = hre;
-  const { deployer } = await getNamedAccounts();
+  const MERKLE_ROOT =
+    "0xe700a492541f4c7e5a5f4e47a71aff038d787285dc87ed00b06da83aefe0fadf";
+  const { deployments } = hre;
   const MAHA = await deployments.get("MAHA");
+  const proxyAdmin = await hre.deployments.get("ProxyAdmin");
   const MigratorProxy = await deployProxy(
     hre,
     "MigratorMaha",
     [MERKLE_ROOT, MAHA.address, LOCKER],
-    deployer,
+    proxyAdmin.address,
     "MigratorMaha"
   );
 
