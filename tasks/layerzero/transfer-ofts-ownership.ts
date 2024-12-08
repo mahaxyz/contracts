bcd67671bbbefd4d5b48b519ecd2db4ea8af95ca
Steven Enamakel <enamakel@maha.xyz>
2024-12-08 17:49:56 +0400

    added script for trasnfering oft ownership

---
+++ tasks/layerzero/transfer-ofts-ownership.ts
@@ -0,0 +1,55 @@
+/**
+  Script to setup OFTs for the token on the various networks.
+
+  npx hardhat transfer-ofts-ownership --network arbitrum --token maha
+  npx hardhat transfer-ofts-ownership --network base --token maha
+  npx hardhat transfer-ofts-ownership --network bsc --token maha
+  npx hardhat transfer-ofts-ownership --network xlayer --token maha
+  npx hardhat transfer-ofts-ownership --network linea --token maha
+  npx hardhat transfer-ofts-ownership --network mainnet --token maha
+  npx hardhat transfer-ofts-ownership --network optimism --token maha
+ */
+import _ from "underscore";
+import { config } from "./config";
+import { task } from "hardhat/config";
+import { waitForTx } from "../../scripts/utils";
+
+task(`transfer-ofts-ownership`)
+  .addParam("token", "either zai or maha")
+  .setAction(async ({ token }, hre) => {
+    const c = config[hre.network.name];
+    if (!c) throw new Error("cannot find connection");
+
+    // await hre.run("setup-oft", { token });
+
+    const [deployer] = await hre.ethers.getSigners();
+    const contractNameToken = token === "zai" ? "ZaiStablecoin" : "MAHA";
+    const contractName = `${contractNameToken}${c.contract}`;
+
+    const oftD = await hre.deployments.get(contractName);
+    const oft = await hre.ethers.getContractAt(
+      "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFT.sol:OFT",
+      oftD.address
+    );
+    const endpoint = await hre.ethers.getContractAt(
+      "IL0EndpointV2",
+      await oft.endpoint()
+    );
+
+    const timelockD = await hre.deployments.get("MAHATimelockController");
+    const safeD = await hre.deployments.get("GnosisSafe");
+
+    const owner = await oft.owner();
+    console.log("\nworking on network", hre.network.name);
+    console.log("current delegate", await endpoint.delegates(oftD.address));
+    console.log("current owner", owner);
+
+    if (owner.toLowerCase() == deployer.address.toLowerCase()) {
+      console.log("\ntransferring ownership to timelock and safe");
+      await waitForTx(await oft.setDelegate(safeD.address));
+      await waitForTx(await oft.transferOwnership(timelockD.address));
+    }
+
+    console.log("\nnew delegate", await endpoint.delegates(oftD.address));
+    console.log("new owner", await oft.owner());
+  });
--- tasks/misc/migrate-timelock-ownership.ts
+++ tasks/misc/migrate-timelock-ownership.ts
@@ -1,13 +1,15 @@
 import { task } from "hardhat/config";
 import { MAHATimelockController } from "../../types";
 import { Addressable, ZeroAddress } from "ethers";
+import { waitForTx } from "../../scripts/utils";
 
 task(`migrate-timelock-ownership`).setAction(async (_, hre) => {
-  const deployments = await hre.deployments.all();
   const [deployer] = await hre.ethers.getSigners();
 
   const safeD = await hre.deployments.get("GnosisSafe");
   const safe = safeD.address;
+
+  const hwWallet = "0xa8E39bBced0E29a07E07826bc61Cc02D597236F2";
 
   const timelockD = await hre.deployments.get("MAHATimelockController");
   const timelock = await hre.ethers.getContractAt(
@@ -19,6 +21,7 @@ task(`migrate-timelock-ownership`).setAction(async (_, hre) => {
   await _checkOrGrant(safe, timelock, "CANCELLER_ROLE");
   await _checkOrGrant(safe, timelock, "EXECUTOR_ROLE");
   await _checkOrGrant(safe, timelock, "PROPOSER_ROLE");
+  await _checkOrGrant(hwWallet, timelock, "PROPOSER_ROLE");
   await _checkOrGrant(ZeroAddress, timelock, "EXECUTOR_ROLE");
   await _checkOrGrant(timelock.target, timelock, "DEFAULT_ADMIN_ROLE");
 
@@ -40,7 +43,7 @@ const _checkOrGrant = async (
     console.log(`  ${role} role already granted to ${rcpt}`);
   } else {
     console.log(`  granting ${role} role to ${rcpt}`);
-    await timelock.grantRole(roleHash, rcpt);
+    await waitForTx(await timelock.grantRole(roleHash, rcpt));
   }
 };
 
@@ -54,6 +57,6 @@ const _checkAndRevoke = async (
   if (await timelock.hasRole(roleHash, rcpt)) {
     console.log(`  ${role} role granted to ${rcpt}`);
     console.log(`  revoking ${role} role from ${rcpt}`);
-    await timelock.revokeRole(roleHash, rcpt);
+    await waitForTx(await timelock.revokeRole(roleHash, rcpt));
   }
 };
